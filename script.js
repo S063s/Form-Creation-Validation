65ae8ff7364893253173c346c331e48dad55a686
Shaun Manda <shaunwmanda@gmail.com>
2025-08-03 15:31:00 +0300

    add a message join in the feedback string

--- script.js
+++ script.js
@@ -33,6 +33,6 @@ if (isValid) {
     feedbackDiv.textContent = "Registration successful!";
     feedbackDiv.style.color = "#28a745";
 } else {
-    feedbackDiv.innerHTML = errorpush;
+    feedbackDiv.innerHTML = messages.join ("<br>");
     feedbackDiv.style.color = "#dc3545";
 }
